ba1895f5c41b790a74725413d8852d23b784dc83
Kcee Landon <landonkcee@gmail.com>
2024-04-24 13:43:33 -0700

    complete exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(s, numReps) {
+    if (numReps < 0) {
+        return 'ERROR';
+    }
+    let result = '';
+    for (let i = 0; i < numReps; i++) {
+        result += s;
+    }
+    return result;
 };
 
 // Do not edit below this line
