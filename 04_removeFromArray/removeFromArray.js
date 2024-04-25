b6d6f246d1cf53804feae5b7723424f339718e8a
Kcee Landon <landonkcee@gmail.com>
2024-04-24 23:18:36 -0700

    complete exercises 3 and 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...extras) {
+    let result = [];
+    arr.forEach((num) => {
+        if (!extras.includes(num)) {
+            result.push(num);
+        }
+    })
+    return result;
 };
 
 // Do not edit below this line
