c216e41c4be82dc6b7246e8d7280587ced0b6eab
Kcee Landon <landonkcee@gmail.com>
2024-04-25 00:44:48 -0700

    complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+    let result = (fahr - 32) * 5 / 9;
+    return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+    let result = (cels * 9) / 5 + 32;
+    return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
