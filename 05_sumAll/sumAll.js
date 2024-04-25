85792ad0d9a56d991e6b6392889cd2d19e2ff89c
Kcee Landon <landonkcee@gmail.com>
2024-04-24 23:43:28 -0700

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
+    let startRange = start;
+    let endRange = end;
+    if (start > end) {
+        startRange = end;
+        endRange = start;
+    }
+    let sum = 0;
+    for (let i = startRange; i <= endRange; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
