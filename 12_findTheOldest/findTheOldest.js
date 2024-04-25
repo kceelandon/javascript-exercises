8167d6408cf1e990385cc7368ae12e029f164cc7
Kcee Landon <landonkcee@gmail.com>
2024-04-25 01:05:03 -0700

    finish exercise 12, moving on

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let indexOfOldest = -1;
+    let oldestAge = -1;
+    for (let i = 0; i < people.length; i++) {
+        let person = people[i];
+        let diff = 0;
+        if (person.yearOfDeath === undefined) {
+            diff = 2024 - person.yearOfBirth;
+        } else {
+            diff = person.yearOfDeath - person.yearOfBirth;
+        }
+        if (diff > oldestAge) {
+            oldestAge = diff;
+            indexOfOldest = i;
+        }
+        console.log(indexOfOldest);
+    }
+    return people[indexOfOldest];
 };
 
 // Do not edit below this line
