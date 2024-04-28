7c73a93f93422859ab6c2e9bf216c715cb3b95a2
Kcee Landon <landonkcee@gmail.com>
2024-04-28 15:31:55 -0700

    complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(first, second) {
+  return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+	return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+	return arr.reduce((total, curr) => {
+    return total + curr;
+  });
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, curr) => {
+    return total * curr;
+  })
 };
 
-const power = function() {
-	
+const power = function(first, second) {
+	return first ** second;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num <= 0) {
+    return 1;
+  }
+  let total = num;
+  num--;
+	while (num > 0) {
+    total *= num;
+    num--;
+  }
+  return total;
 };
 
 // Do not edit below this line
